a40a28cb970daaf92b7dc0d31f59e14f89afa2b0
Jo <zzyang@gudsen.cn>
2022-08-12 14:37:22 +0800

    test: Add WinRegUtil Class Unit Test

---
+++ Test/tst_WinRegUtil.cc
@@ -0,0 +1,45 @@
+#include <QtTest>
+#include <QFileInfo>
+
+// add necessary includes here
+#include "WinReg/WinRegUtil.h"
+#include <iostream>
+
+class tst_WinRegUtil : public QObject
+{
+    Q_OBJECT
+public:
+    tst_WinRegUtil() { }
+    ~tst_WinRegUtil() override { }
+
+private slots:
+    void test_bootUp();
+};
+
+
+void tst_WinRegUtil::test_bootUp()
+{
+    auto path = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+    WinRegUtil winRegUtil;
+
+    auto ret = winRegUtil.bootUpEnabled("HuluMan");
+    QVERIFY(ret);
+
+    auto value = winRegUtil.queryCurrent_UserRegValueFromPath(path, "HuluMan");
+    QFileInfo target(QString::fromStdString(value));
+    QFileInfo source(QCoreApplication::applicationFilePath());
+    QCOMPARE(target.absoluteFilePath(), source.absoluteFilePath());
+
+    ret = winRegUtil.bootUpDisabled("HuluMan");
+    QVERIFY(ret);
+
+    value = winRegUtil.queryCurrent_UserRegValueFromPath(path, "HuluMan");
+    QCOMPARE(value, "");
+}
+
+
+
+
+QTEST_MAIN(tst_WinRegUtil)
+
+#include "tst_WinRegUtil.moc"
